dfbee9adc19bdb5d1923444cd8aa295286a8f514
Alex Suatac <alexsuatac@gmail.com>
2023-12-03 00:09:51 -0500

    day 1 part 2

--- Day1/part2.py
+++ Day1/part2.py
@@ -11,6 +11,15 @@ nums = [
     ("nine" , "9"),
 ]
 
+# find_all taken from 
+# https://stackoverflow.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
+def find_all(str, sub):
+    start = 0
+    while True:
+        start = str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
 
 def process(line):
     # iterate through list
@@ -21,22 +30,22 @@ def process(line):
     for i, c in enumerate(line):
         if c.isdigit():
             pairs.append([i, c])
-        else:
-            for spelled, digit in nums:
-                idx = line.find(spelled)
-                if idx != -1:
-                    pairs.append([idx, digit])
+
+    for spelled, digit in nums:
+        for idx in find_all(line, spelled):
+            pairs.append([idx, digit])
 
 
     # find the smallest and largest index
     # and concatenate the two corresponding digits
-    # returning their sum
-    min_idx = map(lambda *args: min(args), *pairs)
-    max_idx = map(lambda *args: max(args), *pairs)
-    
-
-    return 0
-    # return int(pair[0]+pair[1])
+    # returning their sum - unless there is only one
+    # digit in which case concatenate it with itself
+    if len(pairs) == 0:
+        return 0
+    elif len(pairs) == 1:
+        return int(pairs[0][1] + pairs[0][1])
+    else:
+        return int(min(pairs)[1] + max(pairs)[1])
 
 
 sum = 0
